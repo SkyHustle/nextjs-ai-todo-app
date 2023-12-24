a971e8250e5e1ad5fe16a1b90b2cf88946dd3027
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-24 13:07:12 -0700

    Build add todo form, build loading button component when form is being submitted

--- src/components/AddTodoDialog.tsx
+++ src/components/AddTodoDialog.tsx
@@ -1,7 +1,24 @@
 import { CreateTodoSchema, createTodoSchema } from "@/lib/validation/todo"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog"
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "./ui/dialog"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "./ui/form"
+import { Input } from "./ui/input"
+import { Textarea } from "./ui/textarea"
+import LoadingButton from "./ui/loading-button"
 
 interface AddTodoDialogProps {
     open: boolean
@@ -25,6 +42,53 @@ export default function AddTodoDialog({ open, setOpen }: AddTodoDialogProps) {
                 <DialogHeader>
                     <DialogTitle>Add Todo</DialogTitle>
                 </DialogHeader>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-3"
+                    >
+                        <FormField
+                            name="title"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Todo Title</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="todo title"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name="content"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Todo Content</FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            placeholder="todo content"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <DialogFooter>
+                            <LoadingButton
+                                type="submit"
+                                loading={form.formState.isSubmitting}
+                            >
+                                Submit
+                            </LoadingButton>
+                        </DialogFooter>
+                    </form>
+                </Form>
             </DialogContent>
         </Dialog>
     )
