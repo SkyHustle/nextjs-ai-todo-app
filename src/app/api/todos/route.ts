ad98f9cf66e2393b6f57c5a258c50abb6765abe3
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-24 15:26:09 -0700

    Delete todo api endpoint with zod validation

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,6 +1,10 @@
 // create endpoint to create a new todo
 import prisma from "@/lib/db/prisma"
-import { createTodoSchema, updateTodoSchema } from "@/lib/validation/todo"
+import {
+    createTodoSchema,
+    deleteTodoSchema,
+    updateTodoSchema,
+} from "@/lib/validation/todo"
 import { auth } from "@clerk/nextjs"
 
 export async function POST(req: Request) {
@@ -55,13 +59,14 @@ export async function PUT(req: Request) {
         const { id, title, content } = parseResult.data
 
         const todo = await prisma.todo.findUnique({ where: { id } })
+
         // 404 resource not found
         if (!todo) {
             return Response.json({ error: "Todo not found" }, { status: 404 })
         }
 
         const { userId } = auth()
-        // only authenticated users who create todo can update
+        // only authenticated users who own todo can update
         if (!userId || userId !== todo.userId) {
             return Response.json({ error: "Unauthorized" }, { status: 401 })
         }
@@ -76,6 +81,45 @@ export async function PUT(req: Request) {
 
         // existing resource was updated
         return Response.json({ updatedTodo }, { status: 200 })
+    } catch (error) {
+        console.log(error)
+        return Response.json(
+            { error: "Internal Server Error" },
+            { status: 500 },
+        )
+    }
+}
+
+export async function DELETE(req: Request) {
+    try {
+        const body = req.json()
+
+        const parseResult = deleteTodoSchema.safeParse(body)
+        // if the request body is invalid, return a 400 response
+        if (!parseResult.success) {
+            console.log(parseResult.error)
+            return Response.json({ error: "Invalid Input" }, { status: 400 })
+        }
+
+        const { id } = parseResult.data
+
+        const todo = await prisma.todo.findFirst({ where: { id } })
+
+        // 404 resource not found
+        if (!todo) {
+            return Response.json({ error: "Todo not found" }, { status: 404 })
+        }
+
+        const { userId } = auth()
+        // only authenticated users who own todo can delete
+        if (!userId || userId !== todo.userId) {
+            return Response.json({ error: "Unauthorized" }, { status: 401 })
+        }
+
+        await prisma.todo.delete({ where: { id } })
+
+        // existing resource was deleted
+        return Response.json({ message: "Todo was deleted" }, { status: 200 })
     } catch (error) {
         console.log(error)
         return Response.json(
